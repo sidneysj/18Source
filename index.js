14eb3534597cc1cc22f4141ff2d126ba95527c5d
MrTape <31523513+MrTape@users.noreply.github.com>
2021-04-03 20:48:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,19 +45,19 @@ fs.readdir("./commands/Core/", (err, files) => {
 //   });
 // });
 
-fs.readdir("./commands/Fun/", (err, files) => {
-  if (err) console.error(err);
-  let jsFiles = files.filter(f => f.split(".").pop() === "js");
+// fs.readdir("./commands/Fun/", (err, files) => {
+//   if (err) console.error(err);
+//   let jsFiles = files.filter(f => f.split(".").pop() === "js");
 
-  if (jsFiles.length <= 0) return console.log('\x1b[31m', `[18] There are no commands to load from the Fun folder...`, '\x1b[0m');
+//   if (jsFiles.length <= 0) return console.log('\x1b[31m', `[18] There are no commands to load from the Fun folder...`, '\x1b[0m');
 
-  console.log('\x1b[33m', `[18] Loading ${jsFiles.length} command(s) from the Fun folder...`, '\x1b[0m');
-  jsFiles.forEach((f, i) => {
-    let props = require(`./commands/Fun/${f}`);
-    console.log('\x1b[32m', `[18] ${i + 1}: ${f} loaded!`, '\x1b[0m');
-    client.commands.set(props.name, props);
-  });
-});
+//   console.log('\x1b[33m', `[18] Loading ${jsFiles.length} command(s) from the Fun folder...`, '\x1b[0m');
+//   jsFiles.forEach((f, i) => {
+//     let props = require(`./commands/Fun/${f}`);
+//     console.log('\x1b[32m', `[18] ${i + 1}: ${f} loaded!`, '\x1b[0m');
+//     client.commands.set(props.name, props);
+//   });
+// });
 
 fs.readdir("./commands/Utility/", (err, files) => {
   if (err) console.error(err);
